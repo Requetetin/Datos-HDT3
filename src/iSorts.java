bfae8304a32707c5ab4bd920c84ae3cbfb958032
Requetetin <martinamadog@gmail.com>
2020-02-09 19:16:45 -0600

    Interfaz e inicio de implementacion

---
+++ src/iSorts.java
@@ -0,0 +1,20 @@
+
+
+interface iSorts{
+	public void selectionSort(int data[], int n);
+
+
+	public void mergeSort(int data[], int n);
+
+
+	public void quickSort(int data[], int n);
+
+
+	public void radixSort(int data[]);
+
+
+	public void insertionSort(int data[], int n);
+
+
+
+}
